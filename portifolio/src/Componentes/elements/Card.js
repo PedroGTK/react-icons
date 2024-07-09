f3f524313a7c8793eef4568f3f08aebac3afcee0
Pedro Edilson <pedroedilson27@gmail.com>
2024-07-09 19:56:07 -0300

    2.0

--- portifolio/src/Componentes/elements/Card.js
+++ portifolio/src/Componentes/elements/Card.js
@@ -1,20 +1,39 @@
 import styles from './Card.module.css'
-
+import { useState } from 'react'
 import ButtonB from '../elements/ButtonB'
 
 function Card ({img,title,tech, description, repo, site }){
+    
+    const [info, setInfo] = useState(false)
+
+    function InfoOn(){
+        setInfo(true)
+    }
+
+    function InfoOFF(){
+        setInfo(false)
+    }
+    
+    
+    
     return(
-        <div className ={styles.card} id='card'>
-            <a href={site}>
+        <div  onMouseEnter={InfoOn} onMouseLeave={InfoOFF}  className ={styles.card} id='card'>
+            <a  href={site}>
                 <img src = {img} alt ='Error'></img>
             </a>
 
-            <div className= {styles.info}>
-                <h3> {title} </h3>
-                <p><strong>Tecnologia:</strong>{tech}</p>
-                <p> {description} </p>
-                <ButtonB text='Acesse o repositório' link={repo}/>
-            </div>
+            {
+                info === true  && (
+                    <div className= {styles.info}>
+                        <h3> {title} </h3>
+                        <p><strong>Tecnologia:</strong>{tech}</p>
+                        <p> {description} </p>
+                        <ButtonB text='Acesse o repositório' link={repo}/>
+                    </div>
+                )
+            }
+
+            
 
         </div>
     )
--- portifolio/src/Componentes/section/Presentation.js
+++ portifolio/src/Componentes/section/Presentation.js
@@ -1,14 +1,52 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
+import { useEffect, useState } from 'react'
 
 
 
 function Presentation (){
+
+    const[text, setText] = useState('')
+    const toRotate = ['Pedro Edilson!  ', 'Desenvolvedor Full Stack  ']
+    const [loop, setLoop] = useState(0)
+    const [isDeleting, setIsDeleting] = useState(false)
+    const period = 100;
+    const [delta, setDelta] = useState(50)
+
+    const toType = () =>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+    
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText === ''){
+            setIsDeleting(false)
+            setDelta(period)
+            setLoop(loop+1)
+        }
+    }
+
+    useEffect(()=>{
+
+        let ticker = setInterval(()=>{
+            toType()
+        },delta)
+        return()=>{clearInterval(ticker)}
+
+    },[text])
+
+    
+
+
     return(
         <div className={styles.presentation} id="pesentation">
 
             <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
-            <h1>Olá, eu sou Pedro Edilson!</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p>Sou um apaixonado por tecnologia e soluções inovadoras. <br></br>
                 Por conta disso, estou em transição para a área de desenvolvimento full stack. <br></br>
                 Já concluí diversos projetos e estou sempre em busca de aprender mais sobre programação. <br></br>
